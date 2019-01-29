dcd8d249240ab4ca44920cdf0a3c140157e30dc7
Clément Notin <clement@notin.org>
2019-01-29 15:32:32 +0100

    Make sure that Computer.Name is always full uppercase

--- Sharphound2/JsonObjects/Computer.cs
+++ Sharphound2/JsonObjects/Computer.cs
@@ -4,7 +4,12 @@ namespace Sharphound2.JsonObjects
 {
     internal class Computer : JsonBase
     {
-        public string Name { get; set; }
+        private string _Name;
+        public string Name
+        {
+            get => _Name;
+            set => _Name = value.ToUpper();
+        }
         public string PrimaryGroup { get; set; }
 
         public Dictionary<string, object> Properties = new Dictionary<string, object>();
